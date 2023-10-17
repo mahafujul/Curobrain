dddac18422fc8d095778ba641f5813050f369303
mahafujul <emonmondal083@gmail.com>
2023-10-17 08:26:56 +0530

    Appbar is created

---
+++ src/components/Appbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Appbar(){
+    //A react component which is nothing but a function should return a single element.
+    return(
+        <>
+            
+        </>
+    )
+}
+
+export default Appbar;
