8bd8b06af79429c6098056fdf8045fef5ab4dc7d
mahafujul <emonmondal083@gmail.com>
2023-10-21 19:33:22 +0530

    Since the token is being added with the 'Bearer' prefix, the split function is being used to separate it from the complete token string.

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -1,16 +1,21 @@
 const jwt = require('jsonwebtoken');
 
-const privateKey = 'Lkjhgfdsa'
+const privateKey = "Lkjhgfdsa";
 
 function authentication(req,res,next){
-    const token = req.headers.token;
-    jwt.verify(token, privateKey,(err, decoded)=>{
-        if(err){
-            res.json({message: "Unauthorised token."})
-        }else{
-            next();
-        }
-    })
+    const token = req.headers.token.split(" ")[1]; //Bearer token
+    if(token){
+        jwt.verify(token, privateKey,(err, decoded)=>{
+            if(err){
+                res.json({message: "Unauthorised token."})
+            }else{
+                req.username = decoded.data;
+                next();
+            }
+        })
+    }else{
+        res.json({message: "Token related issue."})
+    }
 }
 
 module.exports = {
