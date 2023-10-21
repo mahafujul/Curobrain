4f71a24593f1fa22fef6aed9ac2c3609fe824bd0
mahafujul <emonmondal083@gmail.com>
2023-10-21 19:49:52 +0530

    Three schemas are being created for admin, user, and course, and Three models are also being created.

--- server/db/index.js
+++ server/db/index.js
@@ -1,4 +1,32 @@
+const mongoose = require('mongoose');
 //Define all the schemas
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    purchasedCourses: [{type: mongoose.Schema.Types.ObjectId, ref: 'Course'}]
+})
 
+const adminSchema = new mongoose.Schema({
+    username: String,
+    password: String
+})
+
+const courseSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imgUrl: String,
+    published: Boolean
+})
 
 //Define all the models/collections.
+
+const User = mongoose.model('User', userSchema)
+const Admin = mongoose.model('Admin', adminSchema)
+const Course = mongoose.model('Course', courseSchema)
+
+module.exports = {
+    User,
+    Admin,
+    Course
+}
