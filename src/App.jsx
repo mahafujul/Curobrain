2ded3e8dfe446132e59fd1426dbb8b8078845128
mahafujul <emonmondal083@gmail.com>
2023-10-22 16:48:02 +0530

    react-router-dom package used for page routing and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
-import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Appbar from './components/Appbar'
+import Signin from './components/Signin'
+import Signup from './components/Signup'
+import Courses from './components/Courses'
 function App() {
  
   return (
-    <>
-      
-    </>
+    <div style={{
+      width:'100vw',
+      height:'100vh',
+      backgroundColor:'#eeeeee'}}>
+      <Router>
+        <Appbar/>
+        <Routes>
+          <Route path={'/'} element={<Signin/>}></Route>
+          <Route path={'/signup'} element={<Signup/>}></Route>
+          <Route path={'/courses'} element={<Courses/>}></Route>
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
