964d8634bf17ac598f3c13b0401abb4595a4d0e8
mahafujul <emonmondal083@gmail.com>
2023-10-28 03:00:37 +0530

    The index.js file was made to connect/import all the components to a single place and export them as a single object, making it simple to import those components anytime we need them.

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Appbar from './Appbar'
+import AddCourse from './AddCourse'
+import Course from './Course'
+import Courses from './Courses'
+import Landing from './Landing'
+import Signin from './Signin'
+import Signup from './Signup'
+
+export {Appbar, AddCourse, Course, Courses, Landing, Signin, Signup}
