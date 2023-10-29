c4d48fca047954ccc1c113dce09bc7bdf11af689
mahafujul <emonmondal083@gmail.com>
2023-10-30 04:16:38 +0530

    A new component is being added

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,5 @@ import Courses from './Courses'
 import Landing from './Landing'
 import Signin from './Signin'
 import Signup from './Signup'
-
-export {Appbar, AddCourse, Course, Courses, Landing, Signin, Signup}
+import EditCourse from './EditCourse'
+export {Appbar, AddCourse, Course, Courses, Landing, Signin, Signup, EditCourse}
