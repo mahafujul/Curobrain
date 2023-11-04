69f5de364b4384eeb45d77b92c7c453acb75affb
mahafujul <emonmondal083@gmail.com>
2023-11-05 01:03:20 +0530

    All required modifications have been made in order to use the Recoil library.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import {Appbar, Signin, Signup, Courses, AddCourse, Course, Landing, EditCourse} from './components/index'
-
+import { RecoilRoot,useSetRecoilState } from 'recoil'
+import { userState } from './store/atoms/user'
 function App() {
  
   return (
@@ -9,19 +10,54 @@ function App() {
       width:'100vw',
       height:'100vh',
       backgroundColor:'#eeeeee'}}>
-      <Router>
-        <Appbar/>
-        <Routes>
-          <Route path={'/'} element={<Landing/>}></Route>
-          <Route path={'/signup'} element={<Signup/>}></Route>
-          <Route path={'/signin'} element={<Signin/>}></Route>
-          <Route path={'/courses'} element={<Courses/>}></Route>
-          <Route path={'/addCourse'} element={<AddCourse/>}></Route>
-          <Route path={'/course/:courseId'} element={<EditCourse/>}></Route>
-        </Routes>
-      </Router>
+      <RecoilRoot>
+        <Router>
+          <Appbar/>
+          <InitUser/>
+          <Routes>
+            <Route path={'/'} element={<Landing/>}></Route>
+            <Route path={'/signup'} element={<Signup/>}></Route>
+            <Route path={'/signin'} element={<Signin/>}></Route>
+            <Route path={'/courses'} element={<Courses/>}></Route>
+            <Route path={'/addCourse'} element={<AddCourse/>}></Route>
+            <Route path={'/course/:courseId'} element={<EditCourse/>}></Route>
+          </Routes>
+        </Router>
+      </RecoilRoot>
     </div>
   )
+}
+
+function InitUser(){
+  const setUser = useSetRecoilState(userState) //Here userState is an atom
+  useEffect(()=>{
+    fetch('http://localhost:3000/me',{
+      method:'GET',
+      headers:{
+        'token': localStorage.getItem('token')
+      }
+    }).then((res)=>{
+      return res.json();
+    }).then((data)=>{
+      if(data.username){
+        setUser({
+          isLoading: false,
+          userEmail: data.username
+        })
+      }else{
+        setUser({
+          isLoading: false,
+          userEmail: null
+        })
+      }
+
+    }).catch((err=>{
+      setUser({
+        isLoading: false,
+        userEmail: null
+      })
+    }))
+  },[])
 }
 
 export default App
