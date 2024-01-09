63b24a07a3d6ffa5be2c86a9867dc60d5a8d8536
mahafujul <emonmondal083@gmail.com>
2024-01-09 22:39:10 +0530

    If there are no courses available to render or display, a warning message (No course present to show.) will appear.

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect,useState } from "react";
-import { Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 
 import {Course} from '../components/index'
 
@@ -17,19 +17,26 @@ function Courses(){
         }).then((res)=>{
             return res.json();
         }).then((data)=>{
-            setCourses(data.courses) 
+            setCourses(data.courses);
         })
     },[])
+
     return(
         <Grid style={{padding:30, display:'flex', paddingTop:'100px'}} container spacing={{ xs: 3, md: 3, lg:2 }} columns={{ xs: 2, sm: 8, md: 12, lg:12, xl:12}}>
             {
                 courses.length!==0? (courses.map((course)=>{
-                    return(
-                        <Grid item xs={2} sm={4} md={4} lg={3} xl={2} key={course._id}>
-                            <Course imgUrl={course.imgUrl} title={course.title} description={course.description} price={course.price} id={course._id}/>
-                        </Grid>
-                    )
-                })):console.log('There is no course present')
+                                        return(
+                                            <Grid item xs={2} sm={4} md={4} lg={3} xl={2} key={course._id}>
+                                                <Course imgUrl={course.imgUrl} title={course.title} description={course.description} price={course.price} id={course._id}/>
+                                            </Grid>
+                                        )
+                                    }))
+                                    :
+                                    (<Box sx={{width:'100vw', height: '100vh',display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                                        <Typography variant="h4" component="h2" sx={{color: '#1976D2'}}>
+                                            No course present to show.
+                                        </Typography>
+                                    </Box>)
             }
         </Grid>       
     )
