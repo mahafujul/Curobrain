1c449531b957399a33aefa0c660f883719d08575
mahafujul <emonmondal083@gmail.com>
2024-01-23 14:29:11 +0530

    Duplicate keys are removed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Box,Typography } from "@mui/material";
 
 function Footer(){
     return(
-        <Box sx={{backgroundColor: '#1976D2', height: '40px', width: '100vw', position: 'fixed', bottom: 0, display: {xs: 'none', sm: 'none',md: 'block', md: 'block', lg: 'block'}, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <Box sx={{backgroundColor: '#1976D2', height: '40px', width: '100vw', position: 'fixed', bottom: 0, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
             <Typography variant='h6' sx={{fontSize:{xs: '20px', md: '30px', lg: '20px', color: 'white'}}}>© {new Date().getFullYear()} Curobrain.</Typography>
         </Box>
     )
