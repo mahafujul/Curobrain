4542dd414e55d74537fa24142e28581c7b06c05e
mahafujul <emonmondal083@gmail.com>
2024-01-23 17:39:23 +0530

    Required modifications are being done for deploying the frontend and backend both on the same port.

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import adminRoute from './routes/admin';
 import userRoute from './routes/user';
-import cors from 'cors';
+// import cors from 'cors';
 import authentication from './middleware/authentication';
 import dotenv from 'dotenv';
 import { connectDB } from './db/connect';
@@ -14,11 +14,16 @@ const port = 3000;
 app.use(express.json()); //we are using this line to parse the incommig requests
 app.use('/admin',adminRoute);
 app.use('/user',userRoute);
-app.use(cors())
+// app.use(cors())
 //we are passing a path and a function/method as arguments on the above method call.
 
 app.get('/me',authentication, (req, res)=>{
     res.json({username: req.headers.username});
+})
+
+app.use(express.static("public"));
+app.use("/*",(req,res)=>{
+    res.sendFile(__dirname+'/public/index.html')
 })
 
 app.listen(port,()=>{
